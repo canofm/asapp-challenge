bbbc818fae0284265a2803c8cda11c9fcffe22d3
canofm <canofedericomartin@gmail.com>
2019-03-06 11:28:53 -0300

    Adding some configuration

---
+++ src/config/index.js
@@ -0,0 +1,32 @@
+import { merge } from "lodash";
+require("dotenv").config();
+
+const env = process.env.NODE_ENV || "development";
+
+const baseConfig = {
+  port: process.env.PORT || 3000,
+  db: {
+    path: process.env.DB_PATH || "/db/challenge.db"
+  },
+  api: {
+    baseUri: process.env.API_SUFFIX || "/api/v0",
+    token: process.env.SECRET_TOKEN || "aToken"
+  }
+};
+
+let envConfig = {};
+
+switch (env) {
+  case "development":
+  case "dev":
+    envConfig = require("./development").config;
+    break;
+  case "prod":
+  case "production":
+    envConfig = require("./production").config;
+    break;
+  default:
+    envConfig = require("./development").config;
+}
+
+export default merge(baseConfig, envConfig);
--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import sqlite3 from "sqlite3";
 import config from "./config";
 
-const dbPath = config.db.path || "../db/challenge.db";
+const dbPath = config.db.path;
 
 export const connect = () => new sqlite3.verbose().Database(dbPath);
