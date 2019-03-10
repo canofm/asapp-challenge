9e1a8a2a939722f3c11f1771baea052badb508ea
canofm <canofedericomartin@gmail.com>
2019-03-10 14:43:09 -0300

    Adding AuthMiddleware to routes

--- src/api/router.js
+++ src/api/router.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { ErrorHandler } from "./error.handler";
 import { UserRouter } from "./v0/routers/user.router";
-// import { authMiddleware } from "./v0/auth";
-// import config from "../config";
+import { authMiddleware } from "./v0/auth";
+import config from "../config";
 import { HealthRouter } from "./v0/routers/health.router";
 import { LoginRouter } from "./v0/routers/login.router";
 
 export const router = express.Router();
 
-// router.use(authMiddleware(config));
 router.use("/check", HealthRouter);
 router.use("/login", LoginRouter);
 router.use("/users", UserRouter);
+router.use(authMiddleware(config));
 router.use(ErrorHandler);
 //TODO: in case of not found it's returning a 400 (bad request)
