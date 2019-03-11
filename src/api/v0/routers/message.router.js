e7703f4ad1e214c3c875eb9b245a323b0e65ba00
canofm <canofedericomartin@gmail.com>
2019-03-10 21:31:11 -0300

    Adding message router

---
+++ src/api/v0/routers/message.router.js
@@ -0,0 +1,8 @@
+import express from "express";
+import MessageAPIFactory from "../factories/message.api.factory";
+
+const messageController = MessageAPIFactory.getController();
+
+export const MessageRouter = express.Router();
+
+MessageRouter.route("/").post((req, res, next) => messageController.create(req, res, next));
