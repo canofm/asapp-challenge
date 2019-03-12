356f83251bab7a3c60cc10f4c832c9ac5a931178
canofm <canofedericomartin@gmail.com>
2019-03-12 18:44:06 -0300

    Improving health check

--- src/api/v0/repositories/health.repository.js
+++ src/api/v0/repositories/health.repository.js
@@ -9,6 +9,7 @@ class HealthRepository {
     return this.db
       .select()
       .from("users")
+      .limit(1)
       .timeout(1000)
       .catch(err => {
         throw new ConnectionDBException(err);
